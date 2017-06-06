f4ec9d8d96cd6c4e8aa1dbdfa5edb95402982dc2
Md. Nadimozzaman Pappo <mnpappo@gmail.com>
2017-06-07 00:35:46 +0600

    final

--- crawler.py
+++ crawler.py
@@ -12,8 +12,8 @@ newspaper_base_url = 'http://www.prothom-alo.com/'
 newspaper_archive_base_url = 'http://www.prothom-alo.com/archive/'
 
 # set the starting and ending date to crawl
-start_date = date(2017, 5, 7)
-end_date = date(2017, 5, 9)
+start_date = date(2014, 1, 12)
+end_date = date(2014, 6, 30)
 delta = end_date - start_date
 
 output_dir = './data-{}-{}'.format(start_date, end_date)
@@ -38,7 +38,7 @@ for i in range(delta.days + 1):
         print '--------------------'
 
         url = newspaper_archive_base_url + str(date_str) + '?edition=print&page=' + str(index + 1)
-        archive_soup = BeautifulSoup(urllib.urlopen(url),"lxml")
+        archive_soup = BeautifulSoup(urllib.urlopen(url))
         all_links = archive_soup.find_all("a", attrs={"class": "link_overlay"})
         # check if this archive page contains any article
         if len(all_links) != 0:
@@ -48,15 +48,19 @@ for i in range(delta.days + 1):
                     urllib.urlopen(article_url).read())
                 container = article_soup.article.div.p.find_all('img')
                 # check if the title length is more then 5
-                if len(container) != 0 and len(container[0].get('title')) > 5:
-                    # cut the sub-string brefore 'l' character to remove image credic
-                    caption = container[0].get('title').rsplit('l', 1)[0]
-                    image_url = container[0].get('src')
-                    image_name = str(image_index)+'.png'
-                    download_by_url = urllib.URLopener()
-                    download_by_url.retrieve(url=image_url, filename=output_dir+'/'+image_name)
-                    result = [image_name, caption]
-                    with open("Output.csv", "ab") as f:
-                        writeFile = csv.writer(f)
-                        writeFile.writerow(result)
-                    image_index += 1
+                try:
+                    if container[0].get('title') is not None:
+                        if len(container[0].get('title')) > 5:
+                            # cut the sub-string brefore 'l' character to remove image credic
+                            caption = container[0].get('title').rsplit('l', 1)[0]
+                            image_url = container[0].get('src')
+                            image_name = str(image_index)+'.png'
+                            download_by_url = urllib.URLopener()
+                            download_by_url.retrieve(url=image_url, filename=output_dir+'/'+image_name)
+                            result = [image_name, caption]
+                            with open("Output.csv", "ab") as f:
+                                writeFile = csv.writer(f)
+                                writeFile.writerow(result)
+                            image_index += 1
+                except Exception:
+                    pass
