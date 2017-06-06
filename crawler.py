179d28287cbe3d217a371fdac6e3d9537de45127
Md. Nadimozzaman Pappo <mnpappo@gmail.com>
2017-06-07 00:14:57 +0600

    file download added

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 import urllib
-import csv
+import csv, os
 from datetime import date, timedelta
 from bs4 import BeautifulSoup
 
@@ -16,9 +16,18 @@ start_date = date(2017, 5, 7)
 end_date = date(2017, 5, 9)
 delta = end_date - start_date
 
+output_dir = './data-{}-{}'.format(start_date, end_date)
+
+try:
+    os.makedirs(output_dir)
+except OSError:
+
+    pass
+
 print '\nScrabbing {}'.format(newspaper_base_url)
 print '\nSaving (image url, caption) from starting date {} to ending date {} of {}\n'.format(start_date, end_date, delta)
 
+image_index = 0
 # get image,caption from starting date to end date
 for i in range(delta.days + 1):
     date_str = start_date + timedelta(days=i)
@@ -29,7 +38,7 @@ for i in range(delta.days + 1):
         print '--------------------'
 
         url = newspaper_archive_base_url + str(date_str) + '?edition=print&page=' + str(index + 1)
-        archive_soup = BeautifulSoup(urllib.urlopen(url))
+        archive_soup = BeautifulSoup(urllib.urlopen(url),"lxml")
         all_links = archive_soup.find_all("a", attrs={"class": "link_overlay"})
         # check if this archive page contains any article
         if len(all_links) != 0:
@@ -42,8 +51,12 @@ for i in range(delta.days + 1):
                 if len(container) != 0 and len(container[0].get('title')) > 5:
                     # cut the sub-string brefore 'l' character to remove image credic
                     caption = container[0].get('title').rsplit('l', 1)[0]
-                    image = container[0].get('src')
-                    result = [caption, image]
+                    image_url = container[0].get('src')
+                    image_name = str(image_index)+'.png'
+                    download_by_url = urllib.URLopener()
+                    download_by_url.retrieve(url=image_url, filename=output_dir+'/'+image_name)
+                    result = [image_name, caption]
                     with open("Output.csv", "ab") as f:
                         writeFile = csv.writer(f)
                         writeFile.writerow(result)
+                    image_index += 1
